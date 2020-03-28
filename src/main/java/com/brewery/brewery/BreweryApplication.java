6a98a2f3995bbb8a401a87d2f73d1f8f871bc1d2
lirdaw <lirdaw@gmail.com>
2020-03-28 19:53:51 +0100

    Initial commit

---
+++ src/main/java/com/brewery/brewery/BreweryApplication.java
@@ -0,0 +1,11 @@
+package com.brewery.brewery;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class BreweryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BreweryApplication.class, args);
+    }
+}
---
+++ src/test/java/com/brewery/brewery/BreweryApplicationTests.java
@@ -0,0 +1,11 @@
+package com.brewery.brewery;
+
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.*;
+
+@SpringBootTest
+class BreweryApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
